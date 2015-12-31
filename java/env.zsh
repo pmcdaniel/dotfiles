4b28ac45cd0d3fc9c431d0b110b4798c8a89c105
Patrick McDaniel <patrick@patrickmcdaniel.com>
2015-12-30 21:18:02 -0500

    Finally remove reference to Java6 and move JAVA_HOME to Java7

--- java/env.zsh
+++ java/env.zsh
@@ -1,5 +1,4 @@
-export JAVA6_HOME=`/usr/libexec/java_home -v 1.6`
 export JAVA7_HOME=`/usr/libexec/java_home -v 1.7`
-export JAVA_HOME=$JAVA6_HOME
+export JAVA_HOME=$JAVA7_HOME
 
 export ANT_HOME=/Users/pmcdaniel/Toolchains/ant/1.9.3
