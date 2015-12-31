1db4e0730b9f26cd746f5fa01768379bb49e5d33
Patrick McDaniel <patrick@patrickmcdaniel.com>
2015-12-30 21:14:28 -0500

    Removed gitflow completions, and add back regular git completion
    I'm not longer using gitflow, and the regular must have been removed
    due to a rebase error

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,8 +1,9 @@
 # Uses git's autocompletion for inner commands. Assumes an install of git's
 # bash `git-completion` script at $completion below (this is where Homebrew
 # tosses it, at least).
-gitflowcompletion="$(brew --prefix)/share/zsh/site-functions/git-flow-completion.zsh"
-if test -f $gitflowcompletion
+completion='$(brew --prefix)/share/zsh/site-functions/_git'
+
+if test -f $completion
 then
-  source $gitflowcompletion
+	source $completion
 fi
