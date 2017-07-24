8d51c710b7b20123c5515914162c7b84ded9878d
Patrick McDaniel <pmcdaniel@lexmark.com>
2017-07-24 09:57:38 -0400

    Change Android path's to reference $HOME
    Before this was hardcoded to my home directory on MacOS

--- android/path.zsh
+++ android/path.zsh
@@ -1,7 +1,7 @@
 # Since paths are loaded before others I'm going to go ahead and export
 # these environment variables so I don't have to redefine them
 
-export ANDROID_HOME=/Users/pmcdaniel/Toolchains/Android
-export ANDROID_NDK_HOME=/Users/pmcdaniel/Toolchains/Android-NDK
+export ANDROID_HOME=$HOME/Toolchains/Android
+export ANDROID_NDK_HOME=$HOME/Toolchains/Android-NDK
 
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME
