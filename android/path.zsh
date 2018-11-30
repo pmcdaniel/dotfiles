aab6b98c17e7060cbbdfc568f2b1cebedf1b82c5
Patrick McDaniel <pmcdaniel@lexmark.com>
2018-11-30 11:29:28 -0500

    Remove android development pathing
    I have not done Android development in a long time so this is not needed

--- android/path.zsh
+++
@@ -1,7 +0,0 @@
-# Since paths are loaded before others I'm going to go ahead and export
-# these environment variables so I don't have to redefine them
-
-export ANDROID_HOME=$HOME/Toolchains/Android
-export ANDROID_NDK_HOME=$HOME/Toolchains/Android-NDK
-
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_NDK_HOME
