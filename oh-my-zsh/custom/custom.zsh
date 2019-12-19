f9ee251d1bd4fa1801fb01bf39812586583b1cfc
Patrick McDaniel <patrick.mcdaniel@lexmark.com>
2019-12-19 17:49:46 -0500

    Add my custom zsh configuration

---
+++ oh-my-zsh/custom/custom.zsh
@@ -0,0 +1,12 @@
+# My custom settings to override oh-my-zsh
+
+export CLICOLOR=1
+export LSCOLORS="exfxcxdxbxegedabagacad"
+
+# My custom keybindings
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+
+# My Custom ZSH options
+setopt NO_LIST_BEEP # Turns beep off for ambiguous completions
+setopt HIST_REDUCE_BLANKS
