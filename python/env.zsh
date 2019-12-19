4f32491479f8876639d354b3d2d56f8ac800860f
Patrick McDaniel <patrick.mcdaniel@lexmark.com>
2019-12-19 12:05:28 -0500

    Remove python topic as I am not using the work in there now

--- python/env.zsh
+++
@@ -1,9 +0,0 @@
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Development
-
-brew_prefix=`brew --prefix`
-
-# Manually setup python to our homebrewed python3 install
-export VIRTUALENVWRAPPER_PYTHON=${brew_prefix}/bin/python3
-
-test -e "${brew_prefix}/bin/virtualenvwrapper.sh" && source "${brew_prefix}/bin/virtualenvwrapper.sh"
