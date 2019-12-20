e9ec0c410aa8b6ffc50e098e03a8c5c7a07d23d1
Patrick McDaniel <patrick.mcdaniel@lexmark.com>
2019-12-19 20:42:57 -0500

    Additional updates to install and main zshrc config

---
+++ install.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Configures git and symlinks files into place
+
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd -P)
+
+set -e
+
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+setup_gitconfig () {
+  if ! [ -f git/gitconfig.local.symlink ]
+  then
+    info 'setup gitconfig'
+
+    git_credential='cache'
+    if [ "$(uname -s)" == "Darwin" ]
+    then
+      git_credential='osxkeychain'
+    fi
+
+    user ' - What is your github author name?'
+    read -e git_authorname
+    user ' - What is your github author email?'
+    read -e git_authoremail
+
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+
+    success 'gitconfig'
+  fi
+}
+
+link_file () {
+    local src=$1 dst=$2
+
+    if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+    then
+        rm -rf "$dst"
+        success "removed $dst"
+    fi
+
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+}
+
+symlink_dotfiles () {
+    info 'symlinking dotfiles'
+
+    for src in $(find -H "$DOTFILES_ROOT" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
+    do
+        dst="$HOME/.$(basename "${src%.*}")"
+        link_file "$src" "$dst"
+    done
+}
+
+setup_gitconfig
+symlink_dotfiles
