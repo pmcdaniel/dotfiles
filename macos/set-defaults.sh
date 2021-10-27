99bd9455f369fc4791d271aa9936555897c8404c
Patrick McDaniel <patrick@patrickmcdaniel.com>
2021-10-27 17:07:27 -0400

    Modify defaults to match current system

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -11,109 +11,131 @@
 # Finder
 ###############################################################################
 
-# Use AirDrop over every interface. srsly this should be a default.
+echo "Use AirDrop over every interface"
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Always open everything in Finder's list view. This is important.
+echo "Always open everything in Finder's list view"
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+echo "Set the Finder prefs for showing a few different volumes on the Desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 
-# Set Home as teh default location for new Finder windows
+echo "Set Home as the default location for new Finder windows"
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-# Show filename extensions
+echo "Show filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# show status bar
+echo "Show status bar in Finder windows"
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# show path bar
+echo "Show path bar in Finder windows"
 defaults write com.apple.finder ShowPathbar -bool true
 
-# avoid creating .DS_Store files on network or USB volumes
+echo "Avoid creating .DS_Store files on network or USB volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
-# automatically open a new Finder window when a volume is mounted
+echo "Automatically open a new Finder window when a volume is mounted"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# Show the ~/Library folder.
+echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
 ###############################################################################
 # General
 ###############################################################################
 
-# Always show scrollbars
+echo "Always show scrollbars"
 defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
 
-# Save screenshots to the desktop
+echo "Save screenshots to the desktop"
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-# Save screenshots in PNG format
+echo "Save screenshots in PNG format"
 defaults write com.apple.screencapture type -string "png"
 
-# Disable shadow in screenshots
+echo "Disable shadow in screenshots"
 defaults write com.apple.screencapture disable-shadow -bool true
+
+echo "Require password immediately after sleep or screen saver beings"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 ###############################################################################
 # Keyboard
 ###############################################################################
 
-# Disable press-and-hold for keys in favor of key repeat.
+echo "Disable press-and-hold for keys in favor of key repeat"
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Set a really fast key repeat.
+echo "Set a really fast key repeat"
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+echo "Enable full keyboard access for all controls"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+###############################################################################
+# Track Pad
+###############################################################################
+
+echo "Enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
 ###############################################################################
 # Dock, Hot Corners, Spaces
 ###############################################################################
 
-# do not automatically rearrange Spaces based on most recent use
+echo "Do not automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
-# do not switch Spaces when opening application windows
+echo "Do not switch Spaces when opening application windows"
 defaults write NSGlobalDomain AppleSpacesSwitchOnActivate -bool false
 
-# automatically hide and show the dock
+echo "Automatically hide and show the dock"
 defaults write com.apple.dock autohide -bool true
 
-# remove the auto-hiding dock delay
+echo "Remove the auto-hiding dock delay"
 defaults write com.apple.dock autohide-delay -float 0
 
-# bottom-left hot corner -> disable
+echo "bottom-left hot corner -> disable"
 defaults write com.apple.dock wvous-bl-corner -int 0
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
-# top-left hot corner -> disable
+echo "top-left hot corner -> disable"
 defaults write com.apple.dock wvous-tl-corner -int 0
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
-# bottom-right hot corner -> put display to sleep
+echo "bottom-right hot corner -> put display to sleep"
 defaults write com.apple.dock wvous-br-corner -int 10
 defaults write com.apple.dock wvous-br-modifier -int 0
 
-# top-right hot corner -> disable
+echo "top-right hot corner -> disable"
 defaults write com.apple.dock wvous-tr-corner -int 0
 defaults write com.apple.dock wvous-tr-modifier -int 0
+
+echo "Hide recent apps from Dock"
+defaults write com.apple.dock show-recents -bool false
+
+echo "Place the Dock on the left side of the screen"
+defaults write com.apple.dock orientation left
 
 ###############################################################################
 # Safari
 ###############################################################################
 
-# Hide Safari's bookmark bar.
+echo "Hide Safari's bookmark bar"
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-# Set up Safari for development.
+echo "Set up Safari for development"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
