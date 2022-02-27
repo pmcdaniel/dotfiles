54dbee04224edc3c92663dd8c767fdfc614a210f
Patrick McDaniel <patrick@patrickmcdaniel.com>
2022-02-27 14:29:52 -0500

    Switch back to using default VIM
    I never really got into configuring neovim and it appears that for the
    time being forces are moving to Microsoft Code

--- oh-my-zsh/plugins/pmcdaniel/pmcdaniel.plugin.zsh
+++ oh-my-zsh/plugins/pmcdaniel/pmcdaniel.plugin.zsh
@@ -13,5 +13,4 @@ alias cls='clear' # Clear screen
 alias pubkey="clipcopy ~/.ssh/id_rsa.pub | echo '=> Public key copied to pasteboard.'"
 
 # Convert vi and vim to nvim
-alias vi="nvim"
-alias vim="nvim"
+alias vi="vim"
