fb89132b6c73651d067916701d8fb2aa30136ddd
Patrick McDaniel <g.patrick.mcdaniel@oracle.com>
2025-09-06 17:41:48 -0400

    Move to blink for completion, simplify configs

--- xdg-config/nvim/lua/plugins/lsp.lua
+++ xdg-config/nvim/lua/plugins/lsp.lua
@@ -1,67 +1,17 @@
 return {
     "neovim/nvim-lspconfig",
-    dependencies = {
-        "stevearc/conform.nvim",
-        "mason-org/mason.nvim",
-        "mason-org/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+    dependencies = { 'saghen/blink.cmp'},
+    opts = {
+        servers = {
+            lua_ls = {}
+        }
     },
-
-    config = function()
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "gopls",
-                "tailwindcss",
-                "clangd",
-            },
-            handlers = {
-                function(server_name) -- default handler
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-            }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-            }, {
-                { name = 'buffer' },
-            })
-        })
+    config = function(_, opts)
+        local lspconfig = require('lspconfig')
+        for server, config in pairs(opts.servers) do
+            config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+            lspconfig[server].setup(config)
+        end
 
         vim.diagnostic.enable = true
         vim.diagnostic.config({
---
+++ xdg-config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,17 @@
+return {
+    'mason-org/mason-lspconfig.nvim',
+    dependencies = {
+        { 'mason-org/mason.nvim', opts = {} },
+        'neovim/nvim-lspconfig',
+    },
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "gopls",
+            "tailwindcss",
+            "clangd",
+            "ruff",
+            "pyright",
+        }
+    }
+}
