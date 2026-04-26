f2bc503d2ca9fdce92a448b4736a7889bbd84c8e
Patrick McDaniel <patrick@patrickmcdaniel.com>
2026-04-26 19:01:05 -0400

    fix(system): Change --color to -G for BSD support

--- system/system_aliases.zsh
+++ system/system_aliases.zsh
@@ -1,7 +1,7 @@
 # Directory aliases
-alias ls="ls -F --color=auto"
-alias ll="ls -lAh --color=auto"
-alias la="ls -AF --color=auto"
+alias ls="ls -FG"
+alias ll="ls -lAhG"
+alias la="ls -AFG"
 
 # Pipe public key to clipboard
 alias pubkey="clipcopy ~/.ssh/id_ed25519.pub | echo '=> Public key copied to pasteboard.'"
