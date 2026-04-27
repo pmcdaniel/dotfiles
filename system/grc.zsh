d85333eb29c40194aaf437f246bcf55e4e6ddc78
Patrick McDaniel <patrick@patrickmcdaniel.com>
2026-04-27 12:43:45 -0400

    fix: Remove ls from grc wrapping

--- system/grc.zsh
+++ system/grc.zsh
@@ -2,4 +2,18 @@
 if (( $+commands[brew] )) && (( $+commands[grc] ))
 then
   source `brew --prefix`/etc/grc.zsh
+
+  _grc_unwrapped_commands=(
+    ls
+  )
+
+  for _grc_unwrapped_command in $_grc_unwrapped_commands
+  do
+    if (( $+functions[$_grc_unwrapped_command] ))
+    then
+      unfunction "$_grc_unwrapped_command"
+    fi
+  done
+
+  unset _grc_unwrapped_command _grc_unwrapped_commands
 fi
